bdf09f47829a0fcd86167611df8550fb932af7c0
Suzi Ryu <ryusu@sheridancollege.ca>
2021-04-16 17:34:21 -0400

    mongo implementation

--- index.js
+++ index.js
@@ -1,78 +1,155 @@
 // import express
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+//const Schema = mongoose.Schema;
 
 app.use(express.json())
  
 // specify the port that your server will run on
 const HTTP_PORT = process.env.PORT || 8080;
 
-let currentItems = [
-    {
-        "name":"Magpie",
-        "rarity":"common",
-        "description":"Gives 9 gold every 4 spins",
-        "goldPerTurn":-1
-    },
-    {
-        "name":"King Midas",
-        "rarity":"rare",
-        "description":"Adds 1 Gold each turn. Adjacent Gold gives 3x more gold.",
-        "goldPerTurn":2
-    },
-    {
-        "name":"Goose",
-        "rarity":"common",
-        "description":"Has a 1% chance of adding a Golden Egg",
-        "goldPerTurn":1
-    },
-    {
-        "name":"Bee",
-        "rarity":"uncommon",
-        "description":"Adjacent Flowers give 2x more gold",
-        "goldPerTurn":1
-    },
-    {
-        "name":"Golden Egg",
-        "rarity":"rare",
-        "description":"",
-        "goldPerTurn":3
-    },
-    {
-        "name":"Cat",
-        "rarity":"common",
-        "description":"",
-        "goldPerTurn":1
-    },
-    {
-        "name":"Void Stone",
-        "rarity":"uncommon",
-        "description":"Adjacent empty squares give 1 coin more. Destroys itself if adjacent to 0  empty squares. Gives 8 coins when destroyed",
-        "goldPerTurn":0
+const mongoURL = "mongodb+srv://dbUser:sqDqsr7UDQWgcZ@@cluster0.8ls97.mongodb.net/Cluster0?retryWrites=true&w=majority"
+
+const connectionOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+
+mongoose.connect(mongoURL, connectionOptions).then(
+    () => {
+        console.log("Connected successfully to your database")
     }
-]
+).catch(
+    (err) => {
+        console.log("Error connecting to the database")
+        console.log(err)
+    }
+)
+
+const Schema = mongoose.Schema;
+
+const CurrentItemsSchema = new Schema({
+    name: String,
+    rarity: String,
+    description: String,
+    goldPerTurn: Number
+ })
+ 
+ const CurrentItem = mongoose.model("current_item_table", CurrentItemsSchema)
+
+//  const c1 = new CurrentItem({"name":"Magpie","rarity":"common","description":"Gives 9 gold every 4 spins","goldPerTurn":-1})
+//  const c2 = new CurrentItem({"name":"King Midas","rarity":"rare","description":"Adds 1 Gold each turn. Adjacent Gold gives 3x more gold.","goldPerTurn":2})
+//  const c3 = new CurrentItem({"name":"Goose","rarity":"common","description":"Has a 1% chance of adding a Golden Egg","goldPerTurn":1})
+//  const c4 = new CurrentItem({"name":"Bee","rarity":"uncommon","description":"Adjacent Flowers give 2x more gold","goldPerTurn":1})
+//  const c5 = new CurrentItem({"name":"Golden Egg","rarity":"rare","description":"","goldPerTurn":3})
+//  const c6 = new CurrentItem({"name":"Cat","rarity":"common","description":"","goldPerTurn":1})
+//  const c7 = new CurrentItem({"name":"Void Stone","rarity":"uncommon","description":"Adjacent empty squares give 1 coin more. Destroys itself if adjacent to 0  empty squares. Gives 8 coins when destroyed","goldPerTurn":0})
+
+
+//  CurrentItem.create([c1,c2,c3,c4,c5,c6,c7]).then(
+//         () => {
+//             console.log("Bulk Insert with create was successful")
+//         }
+//     ).catch(
+//         (err) => {
+//             console.log("Error bulk inserting with create into the table.")
+//             console.log(err)
+//         }
+//     )
+    
+
+//let currentItems = CurrentItem.find()
+
+
+// let currentItems = [
+//     {
+//         "name":"Magpie",
+//         "rarity":"common",
+//         "description":"Gives 9 gold every 4 spins",
+//         "goldPerTurn":-1
+//     },
+//     {
+//         "name":"King Midas",
+//         "rarity":"rare",
+//         "description":"Adds 1 Gold each turn. Adjacent Gold gives 3x more gold.",
+//         "goldPerTurn":2
+//     },
+//     {
+//         "name":"Goose",
+//         "rarity":"common",
+//         "description":"Has a 1% chance of adding a Golden Egg",
+//         "goldPerTurn":1
+//     },
+//     {
+//         "name":"Bee",
+//         "rarity":"uncommon",
+//         "description":"Adjacent Flowers give 2x more gold",
+//         "goldPerTurn":1
+//     },
+//     {
+//         "name":"Golden Egg",
+//         "rarity":"rare",
+//         "description":"",
+//         "goldPerTurn":3
+//     },
+//     {
+//         "name":"Cat",
+//         "rarity":"common",
+//         "description":"",
+//         "goldPerTurn":1
+//     },
+//     {
+//         "name":"Void Stone",
+//         "rarity":"uncommon",
+//         "description":"Adjacent empty squares give 1 coin more. Destroys itself if adjacent to 0  empty squares. Gives 8 coins when destroyed",
+//         "goldPerTurn":0
+//     }
+// ]
  
 app.get("/", (req, res) => {
-    res.send(currentItems)
+    CurrentItem.find().exec().then(
+        (results) => {
+            console.log(results)
+            res.send(results)
+        }
+    ).catch(
+        (err) => {
+            console.log(error)
+            res.status(500).send("Error when getting students from database.")
+        }
+    )
 });
 
 // list of url endpoints that your server will respond to
 app.get("/api/items", (req, res) => {
-    res.send(currentItems)
+    CurrentItem.find().exec().then(
+        (results) => {
+            console.log(results)
+            res.send(results)
+        }
+    ).catch(
+        (err) => {
+            console.log(error)
+            res.status(500).send("Error when getting students from database.")
+        }
+    )
 });
 
 app.get("/api/items/:item_name", (req, res) => {
     console.log(req.params)
-    let name = req.params.item_name;
+    let ItemName = req.params.item_name;
     
-    for (let i = 0; i < currentItems.length; i++) {
-        let item = currentItems[i]
-        if (item.name === name) {
-            return res.send(item)
+    CurrentItem.find({name: ItemName}).exec().then(
+        (results) => {
+            console.log(results)
+            res.status(200).send(results)
         }
-    }
+    ).catch(
+        (err) => {
+            console.log(error)
+            res.status(404).send({msg:`Sorry, could not find item with name: ${name}`})  
+        }
+    )
 
-    res.status(404).send({msg:`Sorry, could not find item with name: ${name}`})   
+     
 });
  
 //add
@@ -81,11 +158,17 @@ app.post("/api/items", (req, res) => {
      console.log(itemToInsert)
 
     // error handling
-    if ("name" in req.body && "rarity" in req.body) {
-        currentItems.push(itemToInsert)
-        res.status(201).send({"msg":"Item successfully inserted!"})
+    if("name" in req.body && "rarity" in req.body) {
+        ItemToAdd = new CurrentItem({"name": req.body.name,
+        "rarity": req.body.rarity,
+        "description": req.body.description,
+        "goldPerTurn" : req.body.goldPerTurn})
+        ItemToAdd.save()
+        res.status(201).send({"msg" : "Item was successfully inserted"})
+    } else {
+        res.status(401).send({"msg":"Sorry, you are missing a item name or rarity in your json payload"})  
     }
-    res.status(401).send({"msg":"Sorry, you are missing a item name or rarity in your json payload"})    
+      
 })
 
 // delete
@@ -94,21 +177,18 @@ app.delete("/api/items/:item_name", (req,res) => {
     let id = req.params.item_name;
     const itemIdFromUser = id
 
-     let pos = undefined
-     for (let i = 0; i < currentItems.length; i++) {
-         if (currentItems[i].name === itemIdFromUser) {
-             pos = i
-             break
-         }
-     }
-
-    if (pos === undefined) {
-        res.status(404).send({"msg":`Could not find item with id of ${itemIdFromUser}`})
-        return
-    }
-
-    currentItems.splice(pos, 1)
-    res.send({"msg":`Deleted item with name of ${itemIdFromUser}`})
+    CurrentItem.deleteOne({name: itemIdFromUser}).exec().then(
+        (results) => {
+            console.log(results)
+            res.status(200).send({"msg":`Deleted item with name of ${itemIdFromUser}`})
+        }
+    ).catch(
+        (err) => {
+            console.log(error)
+            res.status(500).send("Error when getting students from database.")
+        }
+    )
+    
 })
 
 // update...?
